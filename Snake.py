5f01beaea8bce07af5a2e2d9b74ee126c81968d7
Onkar7798 <onkarkajrolkar7798@gmail.com>
2020-08-03 21:04:19 +0530

    Using Pygame

--- Snake.py
+++ Snake.py
@@ -1,27 +1,58 @@
-import turtle
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
 
-pen = turtle.Turtle()
-pen.color("green")
-sc = turtle.getscreen()
-sc.bgcolor("black")
-sc.screensize(200,200)
-pen.up()
-pen.setpos(-400,300)
-pen.down()
-for i in range(0,2):
-	pen.forward(200)
-	pen.penup()
-	pen.right(90)
-	pen.forward(50)
-	pen.right(90)
-	pen.pendown()
-	pen.forward(200)
-	pen.penup()
-	pen.left(90)
-	pen.forward(50)
-	pen.left(90)
-	pen.pendown()
+class cube(object):
+	rows = 0
+	w = 0
+	def __init__(self, start, dirnx = 1, dirny = 0, color = (255,0,0)):
+		pass
 
+	def move(self, dirnx, dirny):
+		pass	
 
+	def draw(self, surface, eyes = False):
+		pass
 
-turtle.done()
+class snake(object):
+	def __init__(self, color, pos):
+		pass
+
+	def move(self):
+		pass
+
+	def reset(self, pos):
+		pass
+
+	def addCube(self):
+		pass
+
+	def draw(self, surface):
+		pass
+
+def drawGrid(w, rows, surface):
+	pass
+
+def redrawWindwo(surface):
+	pass
+
+def randomSnack(rows, items):
+	pass
+
+def message_box(subject, content):
+	pass
+
+def main():
+	pass
+		
+
+# # rows = 
+# # w = 
+# # h = 
+
+# cube.rows = rows
+# cube.w = w
+
+main()
